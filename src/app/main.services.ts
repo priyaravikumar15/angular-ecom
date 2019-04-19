3bf3001e539950ad7c4989a8c2020272c643a31a
priyaravikumar15 <priyaravikumar15@gmail.com>
2019-04-19 14:22:21 +0530

    1 file modified

--- src/app/main.services.ts
+++ src/app/main.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http';
-import { productsinterface } from './products';
+//import { productsinterface } from './products';
 //import { Observable } from 'rxjs/Observable';
 
 @Injectable({providedIn:'root'})
@@ -9,14 +9,15 @@ export class MainServices{
 
 //private productinfo = products[];
 
-private heroesUrl = './product'; 
+//prod: Observable <productsinterface[]>;
+//private heroesUrl = "https://jsonplaceholder.typicode.com"; 
 
   constructor(public http:HttpClient){
-    this.getjsondata().subscribe( data =>this.heroesUrl );
+    //this.getjsondata().subscribe( data => console.log(this.heroesUrl) );
   }
 
   getjsondata(){
     console.log("getjsondata working");
-    return this.http.get(this.heroesUrl);
+    //return this.http.get<productsinterface[]>(this.heroesUrl);
   }
 }
